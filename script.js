01446204967a59341cfac2153ee32ab04d9e3651
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-03-02 23:10:43 -0600

    added window load event and API fetch

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // TODO: add code here
+window.addEventListener('load', function() {
+
+  let result = document.getElementById("container");
+
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+});
