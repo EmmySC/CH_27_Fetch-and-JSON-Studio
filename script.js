83e5e124b16a831ebac1d799aaa6a6ddb6178329
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-03-03 00:11:33 -0600

    added For Loop logic & template literal to make the astronaut container dynamic

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 // TODO: add code here
 window.addEventListener('load', function() {
 
-  let result = document.getElementById("container");
+  const container = document.getElementById("container");
+  //let result = document.getElementById("container");
 
     fetch('https://handlers.education.launchcode.org/static/astronauts.json')
     .then(function (response) {
         return response.json();
     })
-    .then(function(response) {
-        console.log(response);
+    .then(function(data) {
+        console.log(data); //shows the data = an array with 7 astronautObjects
+        for (let i = 0; i < data.length; i++) {
+            let astronaut = data[i];
+        
+        container.innerHTML += ` <!-- use template literal -->
+         <div class="astronaut">
+          <div class="bio">
+             <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+             <ul>
+                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                <li>Active: ${astronaut.active}</li>
+                <li>Skills: ${astronaut.skills.join(", ")}</li>  <!-- .join(" , ") -->
+             </ul>
+          </div>
+          <img class="avatar" src="${astronaut.picture}">
+       </div>
+        `}
     })
 });
